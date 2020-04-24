60d4522f8d3d0717f801ce0011bba768f02d1619
Wilfred Lopez <cubamc@gmail.com>
2020-04-24 01:20:33 -0400

    removed unecesarry files

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,2 +1,2 @@
 /// <reference types="react-scripts" />
-declare module "wilfredlopez/wl-components"
+// declare module "wilfredlopez/wl-components"
--- src/utils/wlTypes.ts
+++
@@ -1,5 +0,0 @@
-export interface WLReactProps {
-  class?: string
-  className?: string
-  style?: { [key: string]: any }
-}
